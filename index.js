429e2b5d94a76072d7165fd4dac22d0dfe0e0e37
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-03-26 13:03:31 -0300

    update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+/**
+ * Tipos de Parâmetro
+ * Query Params: parâmetros nomeados enviados na rota após "?" (filtros, paginação)
+ *               Acesso: const query  = req.query
+ * Route params: parametros para identificar recursos (users/:id))
+ *               Acesso: const params = req.params
+ * Request Body: parametros utilizados para enviar informações
+ */
+
 app.get('/',(req, res)=>{
     return res.json({
         evento: "Semana OmniStack",
