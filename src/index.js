f822d14ff43e0435047bd136108862f1f74a3804
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-03-27 13:44:05 -0300

    update index incidents adding the action join with data ongs and add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const routes = require("./routes");
 
-
 const app = express();
-
+// app.use(cors({origin: 'http://meuapp.com'}));
+app.use(cors());
 app.use(express.json());
 
 /**
