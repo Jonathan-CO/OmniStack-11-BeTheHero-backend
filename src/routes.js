1149e82ea251cc76423c780e3b4e52ebae3886f8
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-03-27 11:31:59 -0300

    Delete incidents and Index Profile

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,18 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async (req, res)=>{
-    const ongs = await connection('ongs').select('*');
-    return res.json(ongs);
-})
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-routes.post('/ongs', async(req, res)=>{
-    const {name, email, whatsapp, city, uf} = req.body;
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
-    const id = crypto.randomBytes(4).toString('HEX');
 
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    })
-    return res.json(id);
-});
-
+routes.get('/profile', ProfileController.index);
 module.exports = routes;
